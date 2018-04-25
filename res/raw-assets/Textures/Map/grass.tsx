190e55ee6728e77e2b43be36ab6d2cac05bfdf6d
edward <rinlavender@163.com>
2018-04-25 11:15:15 +0800

    重新部署

--- res/raw-assets/Textures/Map/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="grass" tilewidth="74" tileheight="74" tilecount="1" columns="1">
- <image source="pics/grass.png" width="80" height="80"/>
-</tileset>
--- res/raw-assets/Textures/Map/leftWall.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="leftWall" tilewidth="74" tileheight="74" tilecount="1" columns="1">
- <image source="pics/leftWall.png" width="74" height="74"/>
- <tile id="0">
-  <properties>
-   <property name="type" value="wall"/>
-  </properties>
- </tile>
-</tileset>
